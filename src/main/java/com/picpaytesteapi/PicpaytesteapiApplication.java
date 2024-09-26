015c3e1e442607c4bc00b7586974f67d89e2e30e
williamjayjay <williamdn@yahoo.com>
2024-09-26 02:38:25 -0300

    feat: initial config

---
+++ src/main/java/com/picpaytesteapi/PicpaytesteapiApplication.java
@@ -0,0 +1,13 @@
+package com.picpaytesteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaytesteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaytesteapiApplication.class, args);
+	}
+
+}
